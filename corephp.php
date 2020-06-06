6ae6b93da0e1a41baa4199771502acf02145dfa5
Developer <ankit15595@gmail.com>
2020-06-06 18:04:22 +0530

    park vehicle action

--- corephp.php
+++ corephp.php
@@ -10,6 +10,30 @@ switch (ACTION) {
         echo "Parking lot created for " . $corephp->getTotalParkingLot() . " vehicles";
     break;
     
+    case 'park_vehicle':
+        $carNumber = $argv[2];
+        $carColor = $argv[3];
+        $total_parking_lot = $corephp->getTotalParkingLot();
+        $getParkedSlots = $corephp->getParkedSlots();
+        if (count($getParkedSlots) < $total_parking_lot) {
+            $isAvailable = $corephp->isCarParked($carNumber);
+            if ($isAvailable > 0) {
+                echo "Car already parked";
+            } else {
+                $setSlot = $corephp->getAvailableSlots();
+                $isSlotFree = $corephp->isFreeSlot($setSlot[0]);
+                if ($isSlotFree === 0) {
+                    $corephp->setCarDetail($carColor, $carNumber, $setSlot[0]);
+                    echo "Allocated slot number " . $setSlot[0];
+                } else {
+                    echo "Slot is not free";
+                }
+            }
+        } else {
+            echo "Sorry, parking lot is full";
+        }
+    break;
+
     case 'destroy_session':
         $getIndex = $corephp->destroySession();
     break;
